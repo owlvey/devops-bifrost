0a15682f0ae2437593dd13327c99e65b4b97827e
Gregory Valderrama Vilca <gcvalderrama@hotmail.com>
2020-06-22 14:56:31 -0500

    add

--- devops-build-local.bat
+++ devops-build-local.bat
@@ -6,3 +6,13 @@ popd
 
 docker tag owlvey/bifrost localhost:48700/owlvey/bifrost
 docker push localhost:48700/owlvey/bifrost
+
+TIMEOUT 5
+kubectl delete pod -n owlvey -l key=owlvey-bifrost-pod 
+TIMEOUT 5
+kubectl get pods -n owlvey -l key=owlvey-bifrost-pod 
+TIMEOUT 10
+kubectl logs -n owlvey deployment/owlvey-bifrost-local-deployment
+
+REM  kubectl exec -it -n owlvey deployment/owlvey-bifrost-local-deployment -- sh
+
